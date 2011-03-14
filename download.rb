31a4687f339da0ece94be105d9a201033fa3bcfa
Valentin Simonov <v@lent.in>
2011-03-15 01:35:06 +0300

    checks if videos folder exists

--- download.rb
+++ download.rb
@@ -4,14 +4,16 @@ require 'open-uri'
 require 'HTTParty'
 
 urls = IO.readlines("data.txt").map {|line| line.chomp}
+if !File.exists?("videos")
+  Dir.mkdir("videos")
+end
 
-count = 1
-urls.each do |url|
+urls.each_with_index do |url, count|
   begin
     doc = Nokogiri.parse(open(url).read)
     node = doc.xpath("//dt/a[text()='Download video to desktop (MP4)']")
     video = "http://www.ted.com" + node.attribute("href").to_s
-    videoName = "videos/(#{count})" + url.match(/http:\/\/www.ted.com\/talks\/(.*)\.html/i)[1] + ".mp4"
+    videoName = "videos/(#{count+1})" + url.match(/http:\/\/www.ted.com\/talks\/(.*)\.html/i)[1] + ".mp4"
     puts "Downloading #{url} to #{videoName}"
 
     File.open( videoName, "w+") do |f|
@@ -20,5 +22,4 @@ urls.each do |url|
   rescue
     puts "Failed to download #{url}"
   end
-  count += 1
 end
